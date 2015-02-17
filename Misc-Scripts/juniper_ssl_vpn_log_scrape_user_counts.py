e5628f3351b9210d6bc6544d1a14f6460ce6577c
bradfears <bfears@gmail.com>
2015-02-16 22:43:35 -0500

    Damn I forgot to save.

--- Misc-Scripts/juniper_ssl_vpn_log_scrape_user_counts.py
+++ Misc-Scripts/juniper_ssl_vpn_log_scrape_user_counts.py
@@ -12,22 +12,22 @@ from __future__ import print_function
 
 infile = "C:\\Users\\bfears\\Downloads\\juniper.cc-prd.access.20150213-a.log"
 
-## Open the file with read only permit
+## Open the file with read only permit.
 f = open(infile, "r")
 
-## Use readlines to read all lines in the file
-## The variable "lines" is a list containing all lines
+## Use readlines to read all lines in the file.
+## The variable "lines" is a list containing all lines.
 lines = f.readlines()
 
-## close the file after reading the lines.
+## Close the file after reading the lines.
 f.close()
 
 ipList = []
 nameList = []
 
 
-## If the file is not empty keep reading line one at a time
-## till the file is empty
+## If the file is not empty, keep reading lines one at a time
+## until the file is empty.
 
 import re
 for index in range(0,len(lines)):
@@ -56,18 +56,18 @@ for index in range(0,len(lines)):
 		nameList.append(userName_stripped)
 			
 
-# Now I have a list (array) of usernames and a list (array) of their IPs.
-# That was act 1.  I hope you enjoyed it.
+## Now I have a list (array) of usernames and a list (array) of their IPs.
+## That was act 1.  I hope you enjoyed it.
+## I need a banana.
 
-# What I need now:
-# 1. Identify the unique usernames
-# 2. Run a counter on the names
+## Other stuff I need now:
+## 1. Identify the unique usernames
+## 2. Run a counter on the names
 
-from collections import Counter
-
-# What to do with each name?
 # I'm missing the obvious.
 # But I found the not-so-obvious.
+
+from collections import Counter
 
 uniqueTotals = Counter(nameList).most_common()
 # I want to find the MOFO who wrote this and have his baby.
