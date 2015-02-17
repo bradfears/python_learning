7a2c4f8d40b4fab524190059402afb59262db2fa
bradfears <bfears@gmail.com>
2015-02-16 22:38:24 -0500

    It works!

--- Misc-Scripts/juniper_ssl_vpn_log_scrape_user_counts.py
+++ Misc-Scripts/juniper_ssl_vpn_log_scrape_user_counts.py
@@ -1,14 +1,21 @@
 from __future__ import print_function
 
+# juniper_ssl_vpn_log_scrape_user_counts.py
+# 2015.02.16 - Brad Fears
+
+# This is a handy tool...
 # pythex - Python regular expression tester
 # http://pythex.org
+
+# I wrote this.
+# Me.  Brad!?  Yeah, that guy.
 
 infile = "C:\\Users\\bfears\\Downloads\\juniper.cc-prd.access.20150213-a.log"
 
 ## Open the file with read only permit
 f = open(infile, "r")
 
-## use readlines to read all lines in the file
+## Use readlines to read all lines in the file
 ## The variable "lines" is a list containing all lines
 lines = f.readlines()
 
@@ -49,11 +56,24 @@ for index in range(0,len(lines)):
 		nameList.append(userName_stripped)
 			
 
-for index in range(0,len(nameList)):
-	if nameList[index] == "NOBODY":		
-		continue;
-	else:
-		print("Username: " + nameList[index])
-		print("Outside IP: " + ipList[index])
-			
-	
+# Now I have a list (array) of usernames and a list (array) of their IPs.
+# That was act 1.  I hope you enjoyed it.
+
+# What I need now:
+# 1. Identify the unique usernames
+# 2. Run a counter on the names
+
+from collections import Counter
+
+# What to do with each name?
+# I'm missing the obvious.
+# But I found the not-so-obvious.
+
+uniqueTotals = Counter(nameList).most_common()
+# I want to find the MOFO who wrote this and have his baby.
+# "Don't be shy...you know you so sexy."
+# I will conceed that my harvesting of IPs was unnecessary, but good learning.
+
+for index in range(0,len(uniqueTotals)):	
+	print(uniqueTotals[index])
+
